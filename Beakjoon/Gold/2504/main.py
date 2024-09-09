430686571a67f8533ccf622f8f1a4506c8f09bf1
ohige01 <ohige01@naver.com>
2024-09-10 00:35:55 +0900

    python: Beakjoon no.2504

--- Beakjoon/Gold/2504/main.py
+++ Beakjoon/Gold/2504/main.py
@@ -13,17 +13,15 @@ for i in range(len(letter)):
             if(letter[i-1]==')' or letter[i-1]==']'):
                 stack_op.append('+')
                 stack_num.append(1)
-        
         # 각 괄호에 맞춰서 연산 추가
         if(letter[i]=='('):
             stack_op.append("*2")
         elif(letter[i]=='['):
             stack_op.append("*3")
-
         stack.append(letter[i])
     # 닫는 괄호를 만나면 pop
     elif(letter[i]==')' or letter[i]==']'):
-        # 스택이 비어있는가 검사
+        # 스택이 비어있는가
         if(len(stack) != 0):
             # 괄호가 올바르게 짝지어져 있는가 검사 
             if(letter[i] == ')'):
@@ -33,22 +31,25 @@ for i in range(len(letter)):
             elif(letter[i] == ']'):
                 if(stack.pop() != '['):
                     state = False
-                    break
-            
+                    break 
             # 연산
-            op = stack_op.pop()  
+            op = stack_op.pop()
             if(op == "*2"):
                stack_num.append(stack_num.pop() * 2) 
             elif(op == "*3"):
                stack_num.append(stack_num.pop() * 3) 
             elif(op == '+'):           
                 stack_num.append(stack_num.pop() + stack_num.pop())
-                op = stack_op.pop()
                 # +연산할 시 뒤에 있는 연산도 함께 실행
+                op = stack_op.pop()
+                # 다수의 +가 있을시 모두 처리
+                while(op == "+"):
+                    stack_num.append(stack_num.pop() + stack_num.pop())
+                    op = stack_op.pop()
                 if(op == "*2"):
                     stack_num.append(stack_num.pop() * 2) 
                 elif(op == "*3"):
-                    stack_num.append(stack_num.pop() * 3)
+                    stack_num.append(stack_num.pop() * 3)                    
         else:
             state = False
             break
