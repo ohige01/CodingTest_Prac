51d0100ef01a6395291c65ba9b1444765d00eb16
ohige01 <ohige01@naver.com>
2024-09-11 01:57:59 +0900

    python: Beakjoon no.11725 주석 작성

--- Beakjoon/Sliver/11725/main.py
+++ Beakjoon/Sliver/11725/main.py
@@ -6,11 +6,14 @@ spot_list = [[] for i in range(N + 1)]
 result = [0] * N
 for i in range(N - 1):
     a, b = map(int, sys.stdin.readline().split())
+    # 인접 리스트 형식
     spot_list[a].append(b)
     spot_list[b].append(a)
 
+# dfs형식으로 그래프 순회
 def findParentNode(node):
     for spot in spot_list[node]:
+        # 저장되지 않은 지점을 만나면 저장
         if(result[spot - 1] == 0):
             result[spot - 1] = node
             findParentNode(spot)
